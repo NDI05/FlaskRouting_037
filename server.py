fa09a553c65aba37c4549f7e68aa09d603bd1533
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-09-01 18:24:50 +0700

    Create New

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, redirect, url_for, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/success/<name>')
+def success(name):
+    return f'Welcome {name}!'
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:  # method is GET
+        user = request.args.get('nm')
+        if user:
+            return redirect(url_for('success', name=user))
+        else:
+            return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
